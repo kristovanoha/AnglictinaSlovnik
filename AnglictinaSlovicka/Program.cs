b0143e30ce5476d95d3da52eec8509d412c3e3e1
kristovanoha <jiri.hegar@centrum.cz>
2015-03-20 07:27:49 +0100

    test

--- AnglictinaSlovicka/Program.cs
+++ AnglictinaSlovicka/Program.cs
@@ -16,6 +16,8 @@ namespace AnglictinaSlovicka
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            //z AA
         }
     }
 }
